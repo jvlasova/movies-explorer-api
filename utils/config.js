4df8a11a648c74381e5dee1497f272427ed88037
Власова <jvlasova@yandex.ru>
2022-11-22 15:07:53 +0100

    Backend_final

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  PORT: process.env.NODE_ENV !== 'production' ? 3000 : process.env.PORT,
+  PORT: process.env.NODE_ENV !== 'production' ? 4000 : process.env.PORT,
   MONGO_URI: process.env.NODE_ENV !== 'production' ? 'mongodb://localhost:27017/moviesdb' : process.env.MONGO_URI,
   JWT_SECRET: process.env.NODE_ENV !== 'production' ? 'JWT_SECRET' : process.env.JWT_SECRET,
 };
