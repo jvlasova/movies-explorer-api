e5f04c7f403c2dc8eae314240e1b2f6a8b08e1db
Власова <jvlasova@yandex.ru>
2022-11-24 14:07:43 +0100

    Backend_final

--- app.js
+++ app.js
@@ -9,12 +9,6 @@ const { limiter } = require('./utils/limiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { errorHandler } = require('./middlewares/errorHandler');
 
-const allowedCors = [
-  'https://jvlasova.movies.nomoredomains.icu',
-  'https://api.jvlasova.movies.nomoredomains.icu',
-  'http://localhost:3000',
-];
-
 const app = express();
 
 app.use(helmet());
@@ -23,25 +17,6 @@ app.use(cookieParser());
 app.use(requestLogger);
 
 app.use(limiter);
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', true);
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
-    return;
-  }
-  next();
-});
 
 app.use('/', router);
 
